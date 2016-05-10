d071d4c6e95e91fd712c3731adddac9a3ab95ff2
Tony <tony@gmail.com>
2016-05-10 22:08:09 +0800

    最终版本

--- showPic/showPic.js
+++ showPic/showPic.js
@@ -5,9 +5,9 @@ function showPic(whichpic) {
     var source=whichpic.getAttribute("href");
     var placeholder=document.getElementById("placeholder")
     placeholder.setAttribute("src",source);
-    var text=whichpic.getAttribute("title");
-    var description=document.getElementById("description");
-    description.firstChild.nodeValue=text;
+    var text=whichpic.getAttribute("title");//当图片库的链接被点击时，这个链接的title属性被提取并保存到text变量中
+    var description=document.getElementById("description");//得到id是description的title元素
+    description.firstChild.nodeValue=text;//把description的title属性设置为text的值
 
 }
 function countBodyChildren() {
